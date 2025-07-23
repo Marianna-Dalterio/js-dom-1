b079082f737e84e42c61c1266f4ff8b5bb9f3485
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-07-23 12:57:27 +0200

    complete exercise and add alternative method

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
-const accendiLamp = document.getElementById("accendi");
-addEventListener(`click`,
-    function () {
-        this.document.getElementById("yellow_lamp").style.display = "block"
-        this.document.getElementById("white_lamp").style.display = "none"
+const bottone = document.getElementById("btn");
+const accendi = document.getElementById("yellow_lamp");
+const spegni = document.getElementById("white_lamp");
+accendi.style.display = "none";
+
+bottone.addEventListener("click", function () {
+    if (accendi.style.display === "none") {
+        accendi.style.display = "block";
+        spegni.style.display = "none";
+        bottone.innerText = "Spegni"
+    } else {
+        accendi.style.display = "none";
+        spegni.style.display = "block";
+        bottone.innerText = "Accendi"
     }
+}
 )
+
+//METODO DEL SRC con str.includes()
+
+// const lamp = document.getElementById("lampadina");
+// const btn = document.getElementById("btn_accendi");
+
+// btn.addEventListener("click", function () {
+//     if (lamp.src.includes("white")) {
+//         lamp.src = "./img/yellow_lamp.png";
+//         btn.innerText = "Spegni";
+//     } else {
+//         lamp.src = "./img/white_lamp.png";
+//         btn.innerText = "Accendi";
+//     }
+// });
+
